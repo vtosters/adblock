f3008312bd2d78fa3de19b15e2a54e4d86aa08df
Artur Skubei <41114720+gdlbo@users.noreply.github.com>
2024-07-05 23:06:05 +0300

    Update adscript.js

--- adscript.js
+++ adscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VK Ads Fixes
 // @namespace    https://vtosters.app/
-// @version      0.2
+// @version      0.3
 // @description  This script applies several fixes to the adblock filter on VK, aiming to speed up site loading and enhance overall performance.
 // @author       gdlbo
 // @match        https://vk.com/*
@@ -12,105 +12,254 @@
 (function () {
     'use strict';
 
-    function modifyVkObject() {
-        if (window.vk) {
-            // Set the ad rotation interval to the maximum safe integer value to prevent ad load
-            Object.defineProperty(window.vk, 'ads_rotate_interval', {
-                value: Number.MAX_SAFE_INTEGER,
-                writable: false,
-            });
+    class InteractionListener {
+        constructor() {
+            this._listeners = [];
+        }
 
-            if (window.vk.audioAdsConfig) {
-                // Disable audio ads and set day limit as reached
-                Object.defineProperties(window.vk.audioAdsConfig, {
-                    'enabled': {
-                        value: false,
-                        writable: false,
-                    },
-                    'sections': {
-                        value: [],
-                        writable: false,
-                    },
-                    'day_limit_reached': {
-                        value: false,
-                        writable: false,
-                    },
-                });
+        addListener(callback) {
+            if (!this._listeners.includes(callback)) {
+                this._listeners.push(callback);
             }
+            return {
+                remove: () => this.removeListener(callback),
+            };
+        }
 
-            // Disable sending user info stats and navigation stats in SPA
-            Object.defineProperties(window.vk.pe, {
-                'send_user_info_stats': {
-                    value: 0,
-                    writable: false,
+        removeListener(callback) {
+            const index = this._listeners.indexOf(callback);
+            if (index !== -1) {
+                this._listeners.splice(index, 1);
+            }
+        }
+
+        get listeners() {
+            return [...this._listeners];
+        }
+    }
+
+    const interaction = new InteractionListener();
+
+    const hookVK = async () => {
+        let vkValue = window.vk;
+
+        Object.defineProperty(window, "vk", {
+            get: () => vkValue,
+            set: (newVk) => {
+                vkValue = newVk;
+                for (const callback of interaction.listeners) {
+                    try {
+                        callback(vkValue);
+                    } catch (e) {
+                        console.error(e);
+                    }
+                }
+                return true;
+            },
+            configurable: true,
+        });
+    };
+
+    let inited = false;
+    const onAddNewCallback = async (callback) => {
+        if (inited) {
+            // await waitVK();
+        } else {
+            inited = true;
+            await hookVK();
+        }
+
+        callback(window.vk);
+    };
+
+    const onChangeVK = (callback) => {
+        const listener = interaction.addListener(callback);
+
+        onAddNewCallback(callback);
+
+        return listener;
+    };
+
+    const createOnChangeVKField = (fieldName) => {
+        const interaction = new InteractionListener();
+
+        const hookField = (vkValue) => {
+            let fieldValue = vkValue[fieldName];
+
+            Object.defineProperty(vkValue, fieldName, {
+                get: () => fieldValue,
+                set: (newValue) => {
+                    fieldValue = newValue;
+                    for (const callback of interaction.listeners) {
+                        try {
+                            callback(fieldValue);
+                        } catch (e) {
+                            console.error(e);
+                        }
+                    }
+                    return true;
                 },
-                'force_send_user_info': {
-                    value: 0,
-                    writable: false,
-                },
-                'send_user_info_on_localhost': {
-                    value: 0,
-                    writable: false,
-                },
-                'send_navigation_stats_in_spa': {
-                    value: 0,
-                    writable: false,
-                },
-                'log_send_user_info_errors': {
-                    value: 0,
-                    writable: false,
-                },
+                configurable: true,
+                enumerable: true,
             });
+        };
 
-            // Set error monitoring config to localhost to prevent sending error data
-            Object.defineProperties(window.vk.cfg.error_monitoring_config, {
-                'dsn': {
-                    value: 'http://127.0.0.1',
-                    writable: false,
-                },
+        const hookFieldWithVK = async () => {
+            let vkValue = window.vk;
+
+            hookField(vkValue);
+
+            onChangeVK((newVk) => {
+                hookField(newVk);
             });
+        };
 
-            // Disable ads and stats to localhost
-            Object.defineProperties(window.vk, {
-                'ads_can_show': {
-                    value: 0,
+        let inited = false;
+        const onAddNewCallback = async (callback) => {
+            if (!inited) {
+                inited = true;
+                await hookFieldWithVK();
+            }
+            callback(window.vk[fieldName]);
+        };
+
+        const onChangeField = (callback) => {
+            const listener = interaction.addListener(callback);
+            onAddNewCallback(callback);
+            return listener;
+        };
+
+        return onChangeField;
+    };
+
+    const onChangeVKPart = createOnChangeVKField("pe");
+    const onChangeAudioAdsConfig = createOnChangeVKField("audioAdsConfig");
+    const onChangeErrorMonitoringConfig = createOnChangeVKField("cfg");
+
+    const modifyVkPart = (vkParts) => {
+        for (const key of [
+            "send_user_info_stats",
+            "force_send_user_info",
+            "send_user_info_on_localhost",
+            "send_navigation_stats_in_spa",
+            "log_send_user_info_errors",
+            "web_mytracker_collect_post_stats",
+            "web_stats_device_id",
+            "web_stats_reduce_debounce",
+            "web_stats_send_beacon",
+            "web_stats_send_on_events_limit",
+            "web_stats_transport_story_view",
+            "sentry_js_web_request_timeouts_feature",
+            "sentry_js_web_request_timeouts_forwarding",
+            "sentry_js_web_timeouts_forwarding",
+            "sentry_js_web_verbose",
+            "sentry_log_network_errors",
+            "ads_app_form_link_redirect",
+            "ads_autopromotion_web_geo",
+            "ads_easy_promote_goods_new_create_api",
+            "ads_light_methods_protection",
+            "ads_market_autopromotion_bookmarks_stats",
+            "ads_use_vk_community_video_portrait_4_5",
+            "clips_web_my_tracker",
+            "feed_post_track_code_client_web",
+            "games_send_track_visitor_activity",
+            "js_errors_no_write_uncaught_errors",
+            "tgb_adblock_protection",
+            "post_adblock_protection_promo",
+        ]) {
+            delete vkParts[key];
+        }
+    };
+
+    const modifyAudioAdsConfig = (audioAdsConfig) => {
+        if (audioAdsConfig) {
+            Object.defineProperties(audioAdsConfig, {
+                'enabled': {
+                    value: false,
                     writable: false,
                 },
-                'leftads': {
-                    value: '',
+                'sections': {
+                    value: [],
                     writable: false,
                 },
-                'wsTransport': {
-                    value: 'http://127.0.0.1',
+                'day_limit_reached': {
+                    value: false,
                     writable: false,
                 },
             });
         }
+    };
 
-        // Check if the AdsLight object exists in the global scope
+    const modifyErrorMonitoringConfig = (errorMonitoringConfig) => {
+        Object.defineProperties(errorMonitoringConfig, {
+            'dsn': {
+                value: 'http://127.0.0.1',
+                writable: false,
+            },
+        });
+    };
+
+    const modifyAdParams = (adParams) => {
+        Object.defineProperties(adParams, {
+            'ads_can_show': {
+                value: 0,
+                writable: false,
+            },
+            'leftads': {
+                value: '',
+                writable: false,
+            },
+            'wsTransport': {
+                value: 'http://127.0.0.1',
+                writable: false,
+            },
+            'ads_rotate_interval': {
+                value: Number.MAX_SAFE_INTEGER,
+                writable: false,
+            }
+        });
+    };
+
+    const modifyWindowProperties = () => {
+        const properties = {
+            noAds: true,
+            noAdsAtAll: true,
+            no_left_ads: true,
+            no_ads: true,
+            isNoAdsForce: true,
+            hide_ad: true,
+            ya_direct: false,
+            yaDirectAdActive: false
+        };
+
+        for (const prop in properties) {
+            if (!(prop in window)) {
+                Object.defineProperty(window, prop, {
+                    value: properties[prop],
+                    writable: false,
+                    configurable: false,
+                    enumerable: false
+                });
+            }
+        }
+    };
+
+    if (window.vk) {
+        modifyVkPart(window.vk.pe);
+        modifyAudioAdsConfig(window.vk.audioAdsConfig);
+        modifyErrorMonitoringConfig(window.vk.cfg.error_monitoring_config);
+        modifyAdParams(window.vk);
+        modifyWindowProperties();
+
+        onChangeVKPart(modifyVkPart);
+        onChangeAudioAdsConfig(modifyAudioAdsConfig);
+        onChangeErrorMonitoringConfig(modifyErrorMonitoringConfig);
+
         if (window.AdsLight) {
-            // Force no ads
             window.AdsLight.isNoAdsForce();
         }
 
-        // Override ads-related functions with empty functions
         window.__adsSet = function () { };
         window.__adsUpdate = function () { };
-
-        // Set flags to disable ads
-        window.noAds = true;
-        window.noAdsAtAll = true;
-        window.no_left_ads = true;
-        window.no_ads = true;
-        window.isNoAdsForce = true;
-        window.hide_ad = true;
-        window.ya_direct = false;
-        window.yaDirectAdActive = false;
     }
-
-    modifyVkObject();
-
-    const interval = window.navigator?.hardwareConcurrency ? 30_000 / navigator.hardwareConcurrency : 30_000;
-
-    setInterval(modifyVkObject, interval);
 })();
