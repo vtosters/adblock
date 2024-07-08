74270146d737ca9112776b739fa2fb182122eb70
Tahgolov <supernovavk@hotmail.com>
2024-07-08 22:20:43 +0300

    update adscript.js

--- adscript.js
+++ adscript.js
@@ -144,82 +144,20 @@
     const onChangeErrorMonitoringConfig = createOnChangeVKField("cfg");
 
     const modifyVkPart = (vkParts) => {
-        for (const key of [
-            "send_user_info_stats",
-            "force_send_user_info",
-            "send_user_info_on_localhost",
-            "send_navigation_stats_in_spa",
-            "log_send_user_info_errors",
-            "web_mytracker_collect_post_stats",
-            "web_stats_device_id",
-            "web_stats_reduce_debounce",
-            "web_stats_send_beacon",
-            "web_stats_send_on_events_limit",
-            "web_stats_transport_story_view",
-            "sentry_js_web_request_timeouts_feature",
-            "sentry_js_web_request_timeouts_forwarding",
-            "sentry_js_web_timeouts_forwarding",
-            "sentry_js_web_verbose",
-            "sentry_log_network_errors",
-            "ads_app_form_link_redirect",
-            "ads_autopromotion_web_geo",
-            "ads_easy_promote_goods_new_create_api",
-            "ads_light_methods_protection",
-            "ads_market_autopromotion_bookmarks_stats",
-            "ads_use_vk_community_video_portrait_4_5",
-            "clips_web_my_tracker",
-            "feed_post_track_code_client_web",
-            "games_send_track_visitor_activity",
-            "js_errors_no_write_uncaught_errors",
-            "tgb_adblock_protection",
-            "post_adblock_protection_promo",
-            "eager_error_monitoring",
-            "mini_apps_performance_close_app_empty_event",
-            "mini_apps_performance_iframe_errors",
-            "mini_apps_performance_web",
-            "mini_apps_send_my_tracker_activity",
-            "post_click_analytics_int_ext_link_click_web",
-            "posting_track_event_count",
-            "unique_adblock_users",
-            "audio_my_tracker_web",
-            "mini_apps_send_stat_arguments_bridge_events_sdk",
-            "ajax_request_parse_html_error",
-            "js_errors_no_write_uncaught_errors",
-            "tns_track_sections",
-            "tns_track_hosts",
-            "geminus_counter",
-            "ads_pixels_track_new_events_web_mvk",
-            "web_navigation_handlers",
-            "measure_module_navigation_stats",
-            "group_join_track_event_count",
-            "feed_content_events_open_post_event_web",
-            "feed_posts_duration_stats_fix",
-            "collect_unsupported_user_info_stats",
-            "log_fetch_requests",
-            "log_fetch_requests_get",
-            "post_adguard_protection_promo",
-            "extended_ajax_logging",
-            "messenger_mediascope_stats_collect",
-            "audio_player_stats_web"
-        ]) {
-            delete vkParts[key];
-        }
+        vkParts.keys()
+            .forEach((key) => {
+                if(isVkPart(key))delete vkParts[key];
+            });
     };
 
     const modifyAudioAdsConfig = (audioAdsConfig) => {
         if (audioAdsConfig) {
             Object.defineProperties(audioAdsConfig, {
                 'enabled': {
-                    value: false,
-                    writable: false,
-                    configurable: false,
-                    enumerable: false
+                    value: false
                 },
                 'day_limit_reached': {
-                    value: false,
-                    writable: false,
-                    configurable: false,
-                    enumerable: false
+                    value: false
                 },
             });
         }
@@ -228,10 +166,7 @@
     const modifyErrorMonitoringConfig = (errorMonitoringConfig) => {
         Object.defineProperties(errorMonitoringConfig, {
             'dsn': {
-                value: 'http://127.0.0.1',
-                writable: false,
-                configurable: false,
-                enumerable: false
+                value: 'http://127.0.0.1'
             },
         });
     };
@@ -239,81 +174,116 @@
     const modifyAdParams = (adParams) => {
         Object.defineProperties(adParams, {
             'ads_can_show': {
-                value: 0,
-                writable: false,
-                configurable: false,
-                enumerable: false
+                value: 0
             },
             'leftads': {
-                value: '',
-                writable: false,
-                configurable: false,
-                enumerable: false
+                value: ''
             },
             'wsTransport': {
-                value: 'http://127.0.0.1',
-                writable: false,
-                configurable: false,
-                enumerable: false
+                value: 'http://127.0.0.1'
             },
             'ads_rotate_interval': {
-                value: Number.MAX_SAFE_INTEGER,
-                writable: false,
-                configurable: false,
-                enumerable: false
+                value: Number.MAX_SAFE_INTEGER
             }
         });
     };
 
     const modifyWindowProperties = () => {
-        const properties = {
-            noAds: true,
-            noAdsAtAll: true,
-            no_left_ads: true,
-            no_ads: true,
-            isNoAdsForce: true,
-            hide_ad: true,
-            ya_direct: false,
-            yaDirectAdActive: false
-        };
-
-        for (const prop in properties) {
-            if (!(prop in window)) {
-                Object.defineProperty(window, prop, {
-                    value: properties[prop],
-                    writable: false,
-                    configurable: false,
-                    enumerable: false
-                });
+        Object.defineProperties(window, {
+            noAds: {
+                value: true
+            },
+            noAdsAtAll: {
+                value: true
+            },
+            no_left_ads: {
+                value: true
+            },
+            no_ads: {
+                value: true
+            },
+            isNoAdsForce: {
+                value: true
+            },
+            hide_ad: {
+                value: true
+            },
+            ya_direct: {
+                value: false
+            },
+            yaDirectAdActive: {
+                value: false
             }
-        }
+        });
     };
 
-    function decodeLink(link) {
-        const httpOffset = link.search("http%3A%2F%2F");
-        const httpsOffset = link.search("https%3A%2F%2F");
-        const postOffset = link.search(/&post=/g);
-        const ccKeyOffset = link.search(/&cc_key=/g);
-        const offset = httpsOffset !== -1 ? httpsOffset : httpOffset;
-
-        if (postOffset === -1 && ccKeyOffset === -1) {
-            return decodeURIComponent(link.substring(offset));
-        } else if (postOffset !== -1 && ccKeyOffset === -1) {
-            return decodeURIComponent(link.substring(offset, postOffset));
-        } else if (postOffset === -1 && ccKeyOffset !== -1) {
-            return decodeURIComponent(link.substring(offset, ccKeyOffset));
-        } else {
-            const endOffset = Math.min(postOffset, ccKeyOffset);
-            return decodeURIComponent(link.substring(offset, endOffset));
-        }
+    function isVkPart(prop) {
+        return (
+            prop === "send_user_info_stats"
+            || prop === "force_send_user_info"
+            || prop === "send_user_info_on_localhost"
+            || prop === "send_navigation_stats_in_spa"
+            || prop === "log_send_user_info_errors"
+            || prop === "web_mytracker_collect_post_stats"
+            || prop === "web_stats_device_id"
+            || prop === "web_stats_reduce_debounce"
+            || prop === "web_stats_send_beacon"
+            || prop === "web_stats_send_on_events_limit"
+            || prop === "web_stats_transport_story_view"
+            || prop === "sentry_js_web_request_timeouts_feature"
+            || prop === "sentry_js_web_request_timeouts_forwarding"
+            || prop === "sentry_js_web_timeouts_forwarding"
+            || prop === "sentry_js_web_verbose"
+            || prop === "sentry_log_network_errors"
+            || prop === "ads_app_form_link_redirect"
+            || prop === "ads_autopromotion_web_geo"
+            || prop === "ads_easy_promote_goods_new_create_api"
+            || prop === "ads_light_methods_protection"
+            || prop === "ads_market_autopromotion_bookmarks_stats"
+            || prop === "ads_use_vk_community_video_portrait_4_5"
+            || prop === "clips_web_my_tracker"
+            || prop === "feed_post_track_code_client_web"
+            || prop === "games_send_track_visitor_activity"
+            || prop === "js_errors_no_write_uncaught_errors"
+            || prop === "tgb_adblock_protection"
+            || prop === "post_adblock_protection_promo"
+            || prop === "eager_error_monitoring"
+            || prop === "mini_apps_performance_close_app_empty_event"
+            || prop === "mini_apps_performance_iframe_errors"
+            || prop === "mini_apps_performance_web"
+            || prop === "mini_apps_send_my_tracker_activity"
+            || prop === "post_click_analytics_int_ext_link_click_web"
+            || prop === "posting_track_event_count"
+            || prop === "unique_adblock_users"
+            || prop === "audio_my_tracker_web"
+            || prop === "mini_apps_send_stat_arguments_bridge_events_sdk"
+            || prop === "ajax_request_parse_html_error"
+            || prop === "js_errors_no_write_uncaught_errors"
+            || prop === "tns_track_sections"
+            || prop === "tns_track_hosts"
+            || prop === "geminus_counter"
+            || prop === "ads_pixels_track_new_events_web_mvk"
+            || prop === "web_navigation_handlers"
+            || prop === "measure_module_navigation_stats"
+            || prop === "group_join_track_event_count"
+            || prop === "feed_content_events_open_post_event_web"
+            || prop === "feed_posts_duration_stats_fix"
+            || prop === "collect_unsupported_user_info_stats"
+            || prop === "log_fetch_requests"
+            || prop === "log_fetch_requests_get"
+            || prop === "post_adguard_protection_promo"
+            || prop === "extended_ajax_logging"
+            || prop === "messenger_mediascope_stats_collect"
+            || prop === "audio_player_stats_web"
+        );
     }
 
     function removeAway() {
         const links = document.getElementsByTagName("a");
-
         for (let i = 0; i < links.length; i++) {
-            if (links[i].href.match(/away.php/)) {
-                links[i].href = decodeLink(links[i].href);
+            const url = new URL(links[i].href);
+            if(url.pathname.endWith("away.php")) {
+                links[i].href = url.searchParams.get("to");
             }
         }
     }
@@ -335,20 +305,17 @@
     }
 
     function clearFunctions(obj) {
-        for (const prop in obj) {
-            if (typeof obj[prop] === 'function') {
+        Object.getOwnPropertyNames(obj)
+            .filter((prop) => typeof obj[prop] === 'function')
+            .forEach((prop) => {
                 try {
                     Object.defineProperty(obj, prop, {
-                        value: function () { },
-                        writable: false,
-                        configurable: false,
-                        enumerable: false
+                        value: function () { }
                     });
                 } catch (error) {
                     console.error(`Failed to clear function: ${prop}`, error);
                 }
-            }
-        }
+            })
     }
 
     if (window.vk) {
