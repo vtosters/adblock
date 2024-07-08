b2cd6bafd25f01a287f94f733ba5bf6389874ec2
Vologhat <supernovavk@hotmail.com>
2024-07-08 22:59:44 +0300

    Small changes to improve readability and code performance (#6)

--- adscript.js
+++ adscript.js
@@ -54,13 +54,13 @@
             get: () => vkValue,
             set: (newVk) => {
                 vkValue = newVk;
-                for (const callback of interaction.listeners) {
+                interaction.listeners.forEach(callback => {
                     try {
                         callback(vkValue);
                     } catch (e) {
                         console.error(e);
                     }
-                }
+                });
                 return true;
             },
             configurable: true,
@@ -69,26 +69,21 @@
 
     let inited = false;
     const onAddNewCallback = async (callback) => {
-        if (inited) {
-            // await waitVK();
-        } else {
+        if (!inited) {
             inited = true;
             await hookVK();
         }
-
         callback(window.vk);
     };
 
     const onChangeVK = (callback) => {
         const listener = interaction.addListener(callback);
-
         onAddNewCallback(callback);
-
         return listener;
     };
 
     const createOnChangeVKField = (fieldName) => {
-        const interaction = new InteractionListener();
+        const fieldInteraction = new InteractionListener();
 
         const hookField = (vkValue) => {
             let fieldValue = vkValue[fieldName];
@@ -97,13 +92,13 @@
                 get: () => fieldValue,
                 set: (newValue) => {
                     fieldValue = newValue;
-                    for (const callback of interaction.listeners) {
+                    fieldInteraction.listeners.forEach(callback => {
                         try {
                             callback(fieldValue);
                         } catch (e) {
                             console.error(e);
                         }
-                    }
+                    });
                     return true;
                 },
                 configurable: true,
@@ -131,7 +126,7 @@
         };
 
         const onChangeField = (callback) => {
-            const listener = interaction.addListener(callback);
+            const listener = fieldInteraction.addListener(callback);
             onAddNewCallback(callback);
             return listener;
         };
@@ -144,178 +139,113 @@
     const onChangeErrorMonitoringConfig = createOnChangeVKField("cfg");
 
     const modifyVkPart = (vkParts) => {
-        for (const key of [
-            "send_user_info_stats",
-            "force_send_user_info",
-            "send_user_info_on_localhost",
-            "send_navigation_stats_in_spa",
-            "log_send_user_info_errors",
-            "web_mytracker_collect_post_stats",
-            "web_stats_device_id",
-            "web_stats_reduce_debounce",
-            "web_stats_send_beacon",
-            "web_stats_send_on_events_limit",
-            "web_stats_transport_story_view",
-            "sentry_js_web_request_timeouts_feature",
-            "sentry_js_web_request_timeouts_forwarding",
-            "sentry_js_web_timeouts_forwarding",
-            "sentry_js_web_verbose",
-            "sentry_log_network_errors",
-            "ads_app_form_link_redirect",
-            "ads_autopromotion_web_geo",
-            "ads_easy_promote_goods_new_create_api",
-            "ads_light_methods_protection",
-            "ads_market_autopromotion_bookmarks_stats",
-            "ads_use_vk_community_video_portrait_4_5",
-            "clips_web_my_tracker",
-            "feed_post_track_code_client_web",
-            "games_send_track_visitor_activity",
-            "js_errors_no_write_uncaught_errors",
-            "tgb_adblock_protection",
-            "post_adblock_protection_promo",
-            "eager_error_monitoring",
-            "mini_apps_performance_close_app_empty_event",
-            "mini_apps_performance_iframe_errors",
-            "mini_apps_performance_web",
-            "mini_apps_send_my_tracker_activity",
-            "post_click_analytics_int_ext_link_click_web",
-            "posting_track_event_count",
-            "unique_adblock_users",
-            "audio_my_tracker_web",
-            "mini_apps_send_stat_arguments_bridge_events_sdk",
-            "ajax_request_parse_html_error",
-            "js_errors_no_write_uncaught_errors",
-            "tns_track_sections",
-            "tns_track_hosts",
-            "geminus_counter",
-            "ads_pixels_track_new_events_web_mvk",
-            "web_navigation_handlers",
-            "measure_module_navigation_stats",
-            "group_join_track_event_count",
-            "feed_content_events_open_post_event_web",
-            "feed_posts_duration_stats_fix",
-            "collect_unsupported_user_info_stats",
-            "log_fetch_requests",
-            "log_fetch_requests_get",
-            "post_adguard_protection_promo",
-            "extended_ajax_logging",
-            "messenger_mediascope_stats_collect",
-            "audio_player_stats_web"
-        ]) {
-            delete vkParts[key];
-        }
+        Object.keys(vkParts).forEach((key) => {
+            if (isVkPart(key)) delete vkParts[key];
+        });
     };
 
     const modifyAudioAdsConfig = (audioAdsConfig) => {
         if (audioAdsConfig) {
             Object.defineProperties(audioAdsConfig, {
                 'enabled': {
-                    value: false,
-                    writable: false,
-                    configurable: false,
-                    enumerable: false
+                    value: false
                 },
                 'day_limit_reached': {
-                    value: false,
-                    writable: false,
-                    configurable: false,
-                    enumerable: false
+                    value: false
                 },
             });
         }
     };
 
     const modifyErrorMonitoringConfig = (errorMonitoringConfig) => {
-        Object.defineProperties(errorMonitoringConfig, {
-            'dsn': {
-                value: 'http://127.0.0.1',
-                writable: false,
-                configurable: false,
-                enumerable: false
-            },
+        Object.defineProperty(errorMonitoringConfig, 'dsn', {
+            value: 'http://127.0.0.1'
         });
     };
 
     const modifyAdParams = (adParams) => {
         Object.defineProperties(adParams, {
             'ads_can_show': {
-                value: 0,
-                writable: false,
-                configurable: false,
-                enumerable: false
+                value: 0
             },
             'leftads': {
-                value: '',
-                writable: false,
-                configurable: false,
-                enumerable: false
+                value: ''
             },
             'wsTransport': {
-                value: 'http://127.0.0.1',
-                writable: false,
-                configurable: false,
-                enumerable: false
+                value: 'http://127.0.0.1'
             },
             'ads_rotate_interval': {
-                value: Number.MAX_SAFE_INTEGER,
-                writable: false,
-                configurable: false,
-                enumerable: false
+                value: Number.MAX_SAFE_INTEGER
             }
         });
     };
 
     const modifyWindowProperties = () => {
-        const properties = {
-            noAds: true,
-            noAdsAtAll: true,
-            no_left_ads: true,
-            no_ads: true,
-            isNoAdsForce: true,
-            hide_ad: true,
-            ya_direct: false,
-            yaDirectAdActive: false
-        };
-
-        for (const prop in properties) {
-            if (!(prop in window)) {
-                Object.defineProperty(window, prop, {
-                    value: properties[prop],
-                    writable: false,
-                    configurable: false,
-                    enumerable: false
-                });
+        Object.defineProperties(window, {
+            noAds: {
+                value: true
+            },
+            noAdsAtAll: {
+                value: true
+            },
+            no_left_ads: {
+                value: true
+            },
+            no_ads: {
+                value: true
+            },
+            isNoAdsForce: {
+                value: true
+            },
+            hide_ad: {
+                value: true
+            },
+            ya_direct: {
+                value: false
+            },
+            yaDirectAdActive: {
+                value: false
             }
-        }
+        });
     };
 
-    function decodeLink(link) {
-        const httpOffset = link.search("http%3A%2F%2F");
-        const httpsOffset = link.search("https%3A%2F%2F");
-        const postOffset = link.search(/&post=/g);
-        const ccKeyOffset = link.search(/&cc_key=/g);
-        const offset = httpsOffset !== -1 ? httpsOffset : httpOffset;
-
-        if (postOffset === -1 && ccKeyOffset === -1) {
-            return decodeURIComponent(link.substring(offset));
-        } else if (postOffset !== -1 && ccKeyOffset === -1) {
-            return decodeURIComponent(link.substring(offset, postOffset));
-        } else if (postOffset === -1 && ccKeyOffset !== -1) {
-            return decodeURIComponent(link.substring(offset, ccKeyOffset));
-        } else {
-            const endOffset = Math.min(postOffset, ccKeyOffset);
-            return decodeURIComponent(link.substring(offset, endOffset));
-        }
+    function isVkPart(prop) {
+        const vkPartsList = [
+            "send_user_info_stats", "force_send_user_info", "send_user_info_on_localhost",
+            "send_navigation_stats_in_spa", "log_send_user_info_errors", "web_mytracker_collect_post_stats",
+            "web_stats_device_id", "web_stats_reduce_debounce", "web_stats_send_beacon",
+            "web_stats_send_on_events_limit", "web_stats_transport_story_view", "sentry_js_web_request_timeouts_feature",
+            "sentry_js_web_request_timeouts_forwarding", "sentry_js_web_timeouts_forwarding", "sentry_js_web_verbose",
+            "sentry_log_network_errors", "ads_app_form_link_redirect", "ads_autopromotion_web_geo",
+            "ads_easy_promote_goods_new_create_api", "ads_light_methods_protection", "ads_market_autopromotion_bookmarks_stats",
+            "ads_use_vk_community_video_portrait_4_5", "clips_web_my_tracker", "feed_post_track_code_client_web",
+            "games_send_track_visitor_activity", "js_errors_no_write_uncaught_errors", "tgb_adblock_protection",
+            "post_adblock_protection_promo", "eager_error_monitoring", "mini_apps_performance_close_app_empty_event",
+            "mini_apps_performance_iframe_errors", "mini_apps_performance_web", "mini_apps_send_my_tracker_activity",
+            "post_click_analytics_int_ext_link_click_web", "posting_track_event_count", "unique_adblock_users",
+            "audio_my_tracker_web", "mini_apps_send_stat_arguments_bridge_events_sdk", "ajax_request_parse_html_error",
+            "js_errors_no_write_uncaught_errors", "tns_track_sections", "tns_track_hosts", "geminus_counter",
+            "ads_pixels_track_new_events_web_mvk", "web_navigation_handlers", "measure_module_navigation_stats",
+            "group_join_track_event_count", "feed_content_events_open_post_event_web", "feed_posts_duration_stats_fix",
+            "collect_unsupported_user_info_stats", "log_fetch_requests", "log_fetch_requests_get",
+            "post_adguard_protection_promo", "extended_ajax_logging", "messenger_mediascope_stats_collect",
+            "audio_player_stats_web"
+        ];
+        return vkPartsList.includes(prop);
     }
 
     function removeAway() {
-        const links = document.getElementsByTagName("a");
+        const links = document.querySelectorAll("a[href*='away.php']");
 
-        for (let i = 0; i < links.length; i++) {
-            if (links[i].href.match(/away.php/)) {
-                links[i].href = decodeLink(links[i].href);
+        links.forEach(link => {
+            const url = new URL(link.href);
+            if (url.pathname.endsWith("away.php")) {
+                const newHref = url.searchParams.get("to");
+                if (newHref) {
+                    link.href = newHref;
+                }
             }
-        }
+        });
     }
 
     function removeElementById(id) {
@@ -335,20 +265,17 @@
     }
 
     function clearFunctions(obj) {
-        for (const prop in obj) {
+        Object.keys(obj).forEach((prop) => {
             if (typeof obj[prop] === 'function') {
                 try {
                     Object.defineProperty(obj, prop, {
-                        value: function () { },
-                        writable: false,
-                        configurable: false,
-                        enumerable: false
+                        value: function () { }
                     });
                 } catch (error) {
                     console.error(`Failed to clear function: ${prop}`, error);
                 }
             }
-        }
+        });
     }
 
     if (window.vk) {
@@ -377,5 +304,5 @@
         if (window.AdsLight) {
             clearFunctions(window.AdsLight);
         }
-    }, window.navigator?.hardwareConcurrency ? (30_000 / navigator.hardwareConcurrency) : 30_000);
+    }, window.navigator?.hardwareConcurrency ? (30000 / navigator.hardwareConcurrency) : 30000);
 })();
